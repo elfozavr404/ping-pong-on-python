718cfd248ac2cfef218f97a25ce10c981d9b7834
elfozavr404 <elfozavrysh@gmail.com>
2025-12-26 20:28:20 +0500

    V1.0.0 Update general code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from pygame import *
-
 mixer.init()
 font.init()
 
@@ -10,6 +9,8 @@ FPS = 60
 clock = time.Clock()
 game = True
 finish = False
+speed_x = 3
+speed_y = 3
 #Music here
 
 class GameSprite(sprite.Sprite):
@@ -42,16 +43,24 @@ class Player(GameSprite):
         if keys_pressed[K_DOWN] and self.rect.y < 480:
            self.rect.y += self.speed
 
+ball = GameSprite(5, 'gde kirpich.png', 405, 180, 100, 100)
 player_1 = Player(7, 'koshak 1.png', 5, 250, 80, 80)
+player_2 = Player(7, 'koshak 2.png', 818, 250, 80, 80)
+
 while game:
-    window.fill((255, 255, 255, 0.92))
+    window.fill((250, 250, 250))
     if finish == False:
         player_1.reset()
         player_1.update_ws()
-
-
-
-
+        player_2.reset()
+        player_2.update_arrows()
+        ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > 400 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(player_1, ball) or sprite.collide_rect(player_2, ball):
+            speed_x *= -1
     for e in event.get():
         if e.type == QUIT:
            game = False
