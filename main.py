78977cba94d714e6ea697ef71feb2d8dc63a298b
elfozavr404 <elfozavrysh@gmail.com>
2025-12-30 20:49:14 +0500

    V3.0.0 Final update!

--- main.py
+++ main.py
@@ -15,11 +15,14 @@ finish = False
 timer_ = True
 speed_x = 3
 speed_y = 3
+sc_1 = 0
+sc_2 = 0
 #Music here
 win_1 = font.render('First Player Win :-D', True, (88, 166, 76))
 win_2 = font.render('Second Player Win :-D', True, (88, 166, 76))
-fake_loading = font.render('Loading...', True, (28, 41, 87))
+fake_loading = font.render('Loading...', True, (127, 46, 46))
 first_time = time2()
+
 
 class GameSprite(sprite.Sprite):
     def __init__(self, speed, picture_name, x, y, width, height):
@@ -40,26 +43,27 @@ class Player(GameSprite):
 
         if keys_pressed[K_w] and self.rect.y > 5:
            self.rect.y -= self.speed
-        if keys_pressed[K_s] and self.rect.y < 480:
+        if keys_pressed[K_s] and self.rect.y < 410:
            self.rect.y += self.speed
+        
 
     def update_arrows(self):
         keys_pressed = key.get_pressed()
 
         if keys_pressed[K_UP] and self.rect.y > 5:
            self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < 480:
+        if keys_pressed[K_DOWN] and self.rect.y < 410:
            self.rect.y += self.speed
 
-ball = GameSprite(5, 'gde kirpich.png', 405, 180, 100, 100)
-player_1 = Player(7, 'koshak 1.png', 5, 250, 80, 80)
-player_2 = Player(7, 'koshak 2.png', 818, 250, 80, 80)
-
+ball = GameSprite(5, 'ed666327dd3ce274d94f2b3547155891.jpg', 405, 180, 100, 100)
+player_1 = Player(7, 'ya.jpg', 5, 250, 80, 80)
+player_2 = Player(7, 'ychtiel.jpg', 818, 250, 80, 80)
 window.blit(fake_loading, (330, 200))
+
 while game:
     if timer_ == True:
         last_time = time2()
-        if last_time - first_time > 3:
+        if last_time - first_time > 2:
             timer_ = False
     if finish == False and timer_ == False:
         window.fill((250, 250, 250))
@@ -70,16 +74,24 @@ while game:
         ball.reset()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
-        if ball.rect.x < 45:
+        score_1 = font.render(str(sc_1), True, (127, 46, 46))
+        score_2 = font.render(str(sc_2), True, (127, 46, 46))
+        window.blit(score_2, (495, 50))
+        window.blit(score_1, (400, 50))
+        if ball.rect.x < 75:
             finish = True
             window.blit(win_2, (160, 250))
-        if ball.rect.x > 758:
+        if ball.rect.x > 730:
             finish = True
             window.blit(win_1, (190, 250))
         if ball.rect.y > 400 or ball.rect.y < 0:
             speed_y *= -1
-        if sprite.collide_rect(player_1, ball) or sprite.collide_rect(player_2, ball):
+        if sprite.collide_rect(player_1, ball):
             speed_x *= -1
+            sc_1 += 1
+        if  sprite.collide_rect(player_2, ball):
+            speed_x *= -1
+            sc_2 += 1
     for e in event.get():
         if e.type == QUIT:
            game = False
@@ -87,4 +99,3 @@ while game:
 
     display.update()
     clock.tick(FPS)
-
